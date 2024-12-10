e3e3cdf30bf8a6ad80a27aee9e413fca7a2d6ab8
Sarita-Rodríguez <saritarodriguezdeJ@gmail.com>
2024-12-10 09:43:27 -0500

    Se acomodo el codigo en AJAX para el buen funcionamiento de la modificacion

--- Ajax/modificarInfo.php
+++ Ajax/modificarInfo.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
-require '../Modelo/modificarInfo.php';
+require '../Modelo/ingresarInformacionM.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
